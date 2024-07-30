99efe3fbdffe87a95bd401370a721ed44fd02a0f
conteK13 <152598360+conteK13@users.noreply.github.com>
2024-07-30 18:44:41 +0900

    Python day 9 : file input, output, module

---
+++ 0730_greet1.py
@@ -0,0 +1,15 @@
+def hello1(name) :
+    x = '%s님 안녕하세요.' % name
+    return x
+    
+def hello2(name) :
+    x = '%s님 반갑습니다.' % name
+    return x
+    
+def hello3(name) :
+    x = '%s님 만나서 반가워요.' % name
+    return x
+
+def sqrt(age) :
+    x = '나이는 %d살 입니다.' % age
+    return x
